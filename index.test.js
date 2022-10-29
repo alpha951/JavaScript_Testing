03b51308102bf4533315324dd63b1cf492a18c7d
Keshav Carpenter <20uec068@lnmiit.ac.in>
2022-10-29 13:43:26 +0530

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -14,7 +14,7 @@ test('Return a sorted object of Days with values.', () => {
     let ansD = { Mon: -6, Tue: 2, Wed: 2, Thu: 4, Fri: 6, Sat: 8, Sun: 2 }
     expect(solution(D)).toEqual(ansD);
     let E = { '2020-01-01': 6, '2020-01-04': 12, '2020-01-05': 14, '2020-01-06': 2, '2020-01-07': 4 }
-    let ansE = { Mon: 2, Tue: 4, Wed: 6, Sat: 12, Sun: 14 }
+    let ansE ={ Mon: 2, Tue: 4, Wed: 6, Thu: 8, Fri: 10, Sat: 12, Sun: 14 };
     expect(solution(E)).toEqual(ansE);
 });
 
